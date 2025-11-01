9f8816dc54735e3187266dd005db2f26ce967b43
johnsonchang-4ever-junior-dev <johnsonchang0728@gmail.com>
2025-11-01 21:25:55 +1100

    finish 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = (num_1, num_2) => {
+	return num_1 + num_2;
 };
 
-const subtract = function() {
-	
+const subtract = (num_1, num_2) => {
+	return num_1 - num_2;
 };
 
-const sum = function() {
-	
+const sum = (arr) => {
+	let acc = 0;
+  for( let i = 0; i < arr.length; i ++){
+    acc += arr[i];
+  }
+  return acc;
 };
 
-const multiply = function() {
-
+const multiply = (arr) => {
+  let multipled = 1;
+  for( let i = 0; i < arr.length; i ++){
+    multipled *= arr[i];
+  }
+  return multipled;
 };
 
-const power = function() {
-	
+const power = (num_1, num_2) => {
+	let powered = 1;
+  for( let i = 0; i < num_2; i ++){
+    powered *= num_1;
+  }
+  return powered;
 };
 
-const factorial = function() {
-	
+const factorial = (num) => {
+	let factor = 1;
+  for( let i = 2; i <= num; i ++){
+    factor *= i;
+  }
+  return factor;
 };
 
 // Do not edit below this line
