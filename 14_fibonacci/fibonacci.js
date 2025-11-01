3ad68ea218990501588decf1eeb28c1d3247fb7c
johnsonchang-4ever-junior-dev <johnsonchang0728@gmail.com>
2025-11-01 21:58:06 +1100

    finish 14, 15, 16

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    if (!Number.isInteger(n)){
+        n = parseInt(n)
+    }
+    
+    if (n < 0) return "OOPS";
+
+    if (n <= 1) {
+        return n;
+    }
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 
 };
 
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = (arr) => {
+    const currentYear = new Date().getFullYear()
+    arr = arr.sort((a , b) =>  {
+        const aDeath = a.yearOfDeath || currentYear;
+        const bDeath = b.yearOfDeath || currentYear;
 
+        const aAge = aDeath - a.yearOfBirth;
+        const bAge = bDeath - b.yearOfBirth;
+
+        return bAge - aAge;
+    });
+    return arr[0];
 };
 
 // Do not edit below this line
