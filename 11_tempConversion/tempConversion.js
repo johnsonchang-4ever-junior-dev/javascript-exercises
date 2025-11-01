b3cc770f9c4c518ded1c64bed6cb8638ce4f3631
johnsonchang-4ever-junior-dev <johnsonchang0728@gmail.com>
2025-11-01 21:17:56 +1100

    finish 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = (fahrenheit)=> {
+  const celsius = (fahrenheit  - 32 ) / 9 * 5 ;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = (celsius) => {
+  const fahrenheit = (celsius * 9 / 5) + 32;
+  return Math.round(fahrenheit * 10) / 10;;;
 };
 
 // Do not edit below this line
