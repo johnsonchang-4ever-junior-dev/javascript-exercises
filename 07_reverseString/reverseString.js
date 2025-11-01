0eb6f81b80b5fd05089a63dd1ec3cdceec2a568b
johnsonchang-4ever-junior-dev <johnsonchang0728@gmail.com>
2025-11-01 20:04:14 +1100

    finish 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
+const reverseString = (string) => {
+    // or use 
+    // s.split("").reverse().join("");
+    // .split("") split into array
+    // .reverse() reverse array
+    // .join("") combine an array .join() does not change the array — it returns a new string.
+    if(string.length === 0) return string;
+    let characters = []
+    for(let i = (string.length - 1); i >= 0; i--){
+        characters.push(string[i]);
+    }
+
+    return characters.join("");
 
 };
+
+// let outcome = reverseString("abc");
+
+// console.log(outcome);
 
 // Do not edit below this line
 module.exports = reverseString;
