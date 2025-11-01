d88f0e4047ebba46a9fb8b978da22e79a66fe30d
johnsonchang-4ever-junior-dev <johnsonchang0728@gmail.com>
2025-11-01 21:38:59 +1100

    finish 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = (raw) => {
 
-};
+    let inputTxt = raw.toLowerCase().replace(/[^a-z0-9]/g, '')
+    let reverseText = inputTxt.split("").reverse().join("");
+    return reverseText === inputTxt;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
