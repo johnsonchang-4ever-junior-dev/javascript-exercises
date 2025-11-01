df11a4525349aa755dce0c180ff1438e69c4e2bd
johnsonchang-4ever-junior-dev <johnsonchang0728@gmail.com>
2025-11-01 20:58:57 +1100

    finish 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = (num_1, num_2) => {
 
+    if( ! Number.isInteger(num_1) || !Number.isInteger(num_2) ) {
+        return "ERROR";
+    }
+    if(num_1 < 0 || num_2 < 0){
+        return "ERROR";
+    }
+    
+    if (num_1 > num_2){
+        let num = num_1;
+        num_1 = num_2;
+        num_2 = num;
+    }
+
+
+    let sum = 0;
+    for (let i = num_1; i <= num_2; i++){
+        sum += i
+    }
+    return sum;
 };
+
+// console.log(sumAll(10, "90"));
 
 // Do not edit below this line
 module.exports = sumAll;
